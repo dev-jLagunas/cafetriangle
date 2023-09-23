84a4e7ebd23902379f4c41d551c3a81f4f2aacf5
Juan Lagunas <dev.jlagunas@gmail.com>
2023-09-23 13:14:09 +0900

    Menu features completed

--- src/app/menu-full/menu-full.component.ts
+++ src/app/menu-full/menu-full.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-full',
@@ -6,58 +7,183 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-full.component.css'],
 })
 export class MenuFullComponent {
+  constructor(private router: Router) {}
+
   menuItems: MenuItem[] = [
     {
       id: 1,
       title: 'Chicken Burrito',
-      category: 'Category 1',
-      price: 10.99,
+      category: 'Lunch',
+      price: 1200,
       img: 'assets/store-photos/chicken-burrito.jpg',
-      description: 'Description of Item 1',
+      description:
+        'Tender chicken drenched in a tomato based red sauce filled with spices',
     },
     {
       id: 2,
       title: 'Flan (pudding)',
-      category: 'Category 2',
-      price: 12.99,
+      category: 'Dessert',
+      price: 550,
       img: 'assets/food/flan-niceshot.jpg',
-      description: 'Description of Item 2',
+      description:
+        'Rich Mexican style pudding with a dark caramel sauce and raspberry topping.',
     },
     {
       id: 3,
-      title: 'Blueberry New York Cheesecake',
-      category: 'Category 2',
-      price: 12.99,
-      img: 'assets/food/cheesecake.jpg',
-      description: 'Description of Item 2',
+      title: 'Matcha Crumble Cake',
+      category: 'Dessert',
+      price: 450,
+      img: 'assets/food/matcha-crumble.jpg',
+      description: 'Match crumble cake topped with a raspberry homemade jam.',
     },
     {
       id: 4,
       title: 'Vegan Choco Chip Cookies',
-      category: 'Category 2',
-      price: 12.99,
+      category: 'Dessert',
+      price: 300,
       img: 'assets/store-photos/vegan-cookies.jpg',
-      description: 'Description of Item 2',
+      description:
+        'Simple but delicious vegan chocolate chip cookies for your soul.',
     },
     {
       id: 5,
-      title: 'Vegan Red Velvet',
-      category: 'Category 2',
-      price: 12.99,
-      img: 'assets/store-photos/red-velvet.jpg',
-      description: 'Description of Item 2',
+      title: 'Carnitas Burrito',
+      category: 'Lunch',
+      price: 1200,
+      img: 'assets/food/carnitas-burrito.jpg',
+      description:
+        '3 hours simmered pork infused with various spices and juices. Very tender!',
     },
     {
-      id: 4,
-      title: 'Lemon Coconut Poundcake',
-      category: 'Category 2',
-      price: 12.99,
-      img: 'assets/food/lemon-poundcake.jpg',
-      description: 'Description of Item 2',
+      id: 6,
+      title: 'Quesadilla',
+      category: 'Lunch',
+      price: 800,
+      img: 'assets/food/quesadillas.jpg',
+      description:
+        'Chicken or regular quesadilla available. Sides options also available.',
     },
-
-    // Add more items here as needed
+    {
+      id: 7,
+      title: 'Tres Leches Cake',
+      category: 'Dessert',
+      price: 600,
+      img: 'assets/food/tres-cake.jpg',
+      description:
+        'Our most popular dessert. A traditional Mexican spongecake soaked in milks.',
+    },
+    {
+      id: 8,
+      title: 'Banana Bread',
+      category: 'Dessert',
+      price: 550,
+      img: 'assets/food/banana-bread.jpg',
+      description:
+        'Simple vegan banana bread with earl grey. A la mode available.',
+    },
+    {
+      id: 9,
+      title: 'Carrot Cupcake',
+      category: 'Dessert',
+      price: 500,
+      img: 'assets/food/carrot-cupcake.jpg',
+      description:
+        'Perfectly spiced and sweetened with vegan buttercream frosting cupcake.',
+    },
+    {
+      id: 10,
+      title: 'Jamaica Soda',
+      category: 'Drinks',
+      price: 550,
+      img: 'assets/drinks/Hibiscus.jpg',
+      description:
+        'A refreshing drink made with hibiscus syrup mixed with soda water',
+    },
+    {
+      id: 10,
+      title: 'Mexican Hot Chocolate',
+      category: 'Drinks',
+      price: 600,
+      img: 'assets/drinks/hot-choco.jpg',
+      description:
+        'Authentic Mexican hot chocolate. Not too sweet with a hint of spices.',
+    },
+    {
+      id: 11,
+      title: 'Cafe Latte',
+      category: 'Drinks',
+      price: 600,
+      img: 'assets/drinks/cafe-latte.jpg',
+      description:
+        'Made with custom bean blend. Short/Tall/Hot/Iced and Soy available.',
+    },
+    {
+      id: 12,
+      title: 'Matcha Latte',
+      category: 'Drinks',
+      price: 600,
+      img: 'assets/drinks/matcha-latte.jpg',
+      description:
+        'Vibrant green tea latte. Short/Tall/Hot/Iced and Soy available.',
+    },
+    {
+      id: 12,
+      title: 'Black Sesame Latte',
+      category: 'Drinks',
+      price: 600,
+      img: 'assets/drinks/black-latte.jpg',
+      description:
+        'Unique black sesame latte. Short/Tall/Hot/Iced and Soy available.',
+    },
+    {
+      id: 13,
+      title: 'Chai Latte',
+      category: 'Drinks',
+      price: 600,
+      img: 'assets/drinks/chai-drip.jpg',
+      description:
+        'Made with our special blend of spices. Great sweet & spicy balance.',
+    },
+    {
+      id: 14,
+      title: 'Earl Grey Tea',
+      category: 'Drinks',
+      price: 600,
+      img: 'assets/drinks/earl-tea.jpg',
+      description:
+        'A soft but vibrant flavor and smell. Short/Tall/Hot/Iced all available.',
+    },
+    {
+      id: 15,
+      title: 'Americano/Blend Coffee',
+      category: 'Drinks',
+      price: 600,
+      img: 'assets/drinks/americano.jpg',
+      description:
+        'Made with locally roasted beans. Dark roast with 3 countries beans.',
+    },
+    {
+      id: 16,
+      title: 'Espresso Shot',
+      category: 'Drinks',
+      price: 600,
+      img: 'assets/drinks/espresso.jpg',
+      description:
+        'Made with locally roasted beans. Dark roast with 3 countries beans.',
+    },
   ];
+
+  filteredItems: MenuItem[] = this.menuItems.slice();
+
+  filterItems(category: string) {
+    if (category === 'all') {
+      this.filteredItems = this.menuItems.slice();
+    } else {
+      this.filteredItems = this.menuItems.filter(
+        (item) => item.category === category
+      );
+    }
+  }
 }
 
 // Define a TypeScript interface for the menu item objects
---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+})
+export class ModalComponent {}
